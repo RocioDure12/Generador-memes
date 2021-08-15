aabc26c37720b0f3bf2c57fc3cac08d664563638
Rocio Duré <rocioevelyndure@gmail.com>
2021-08-15 01:10:52 -0300

    funciones

--- main.js
+++ main.js
@@ -13,11 +13,13 @@ const body= document.querySelector("body")
 }
 
 //FUNCION PARA CARGAR IMAGEN
-const inputImagen = document.querySelector(".input-imagen")
 const imagenMeme = document.querySelector(".imagen-meme")
+const inputImagen = document.querySelector(".input-imagen")
 
     inputImagen.oninput= () => {
-        imagenMeme.src = inputImagen.value
+    
+    
+    imagenMeme.src = inputImagen.value
    }
 
 //FUNCION PARA BOTON DE IMAGEN Y TEXTO
@@ -44,7 +46,9 @@ const textoInferior=document.querySelector(".texto-inferior")
 const inputTextoInferior=document.querySelector(".input-texto-inferior")
 
     inputTextoSuperior.oninput=()=>{
+
         textoSuperior.textContent=inputTextoSuperior.value
+        
 
 }
 
@@ -52,4 +56,40 @@ const inputTextoInferior=document.querySelector(".input-texto-inferior")
         textoInferior.textContent=inputTextoInferior.value
 
     }
-       
+
+    /*
+
+document.querySelector(".Brillo").onchange= aplicarFiltros
+document.querySelector(".Opacidad")
+document.querySelector(".Contraste")
+document.querySelector(".Desenfoque")
+document.querySelector(".Escala-de-grises")
+document.querySelector(".Sepia")
+document.querySelector(".Hue")
+document.querySelector(".Negativo")
+
+
+const aplicarFiltros=()=>{
+ 
+}*/
+//Funcion para seleccionar fuente
+const selectFuente=document.getElementById("select-fuente")
+selectFuente.onchange=()=>{
+    textoSuperior.style.fontFamily= selectFuente.value
+    textoInferior.style.fontFamily=selectFuente.value
+}
+
+//funcion para ocultar o mostrar texto superior e inferior
+const checkboxTextoInferior=document.querySelector(".checkbox-texto-inferior")
+checkboxTextoInferior.onchange =()=>{
+    if (checkboxTextoInferior.checked){
+        textoInferior.style.display="none"
+    }
+    else {textoInferior.style.display="block"}
+}
+
+const inputBrillo=document.querySelector(".Brillo")
+inputBrillo.onchange=()=>{
+    imagenMeme.style.filter=`brightness(${inputBrillo.value})`
+    
+}
